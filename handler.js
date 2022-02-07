61a0c1a20db14b80d2887bd9453053e0d6ba300f
mannan-bjit <mannan.bhuiyan@bjitgroup.com>
2022-02-07 23:37:12 +0600

    deploy lambda

--- handler.js
+++ handler.js
@@ -4,4 +4,3 @@ module.exports.generateRandomNumber = event => {
   const randomNumber = parseInt(Math.random() * 100);
   console.log("The random generated integer is: ", randomNumber);
   return randomNumber;
-};
