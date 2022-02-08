f0fdd16a247fe7e7db82f0236f10070ccca1dc57
mannan-bjit <mannan.bhuiyan@bjitgroup.com>
2022-02-08 20:39:45 +0600

    serverless lambda

--- handler.js
+++ handler.js
@@ -4,3 +4,4 @@ module.exports.generateRandomNumber = event => {
   const randomNumber = parseInt(Math.random() * 100);
   console.log("The random generated integer is: ", randomNumber);
   return randomNumber;
+};
